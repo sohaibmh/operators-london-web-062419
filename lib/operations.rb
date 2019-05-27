55bcbcca9d30edabfab8b7dd22d1782a1b070d96
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-05-27 16:17:47 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,18 +10,3 @@ end
 def not_safe?(speed)
 speed < 40 or speed > 60 ? true : false
 end
-
-name = "w"
-case name
-
-  when "Alice"
-    puts "Hello, Alice!"
-  when "The White Rabbit"
-    puts "Don't be late, White Rabbit"
-  when "The Mad Hatter"
-    puts "Welcome to the tea party, Mad Hatter"
-  when "The Queen of Hearts"
-    puts "Please don't chop off my head!"
-  else
-    puts "Whoooo are you?"
-end
